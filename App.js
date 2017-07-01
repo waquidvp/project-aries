c0fbc8c127c48af3259bb6638335881a8d5d1612
Waquid Valiya Peedikakkal <waquid@idom.io>
2017-07-01 23:18:23 +0100

    Added app entry and changed name in package.json

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default class App extends React.Component {
+    render(){
+        return(
+            <Text>App renders</Text>
+        );
+    }
+}
