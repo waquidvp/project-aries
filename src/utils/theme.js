a446794d970cd8f1d6c8dbd8d46831d7f2e05bd4
Waquid Valiya Peedikakkal <waquidvp@gmail.com>
2017-07-09 15:51:44 +0100

    Add themeing support

---
+++ src/utils/theme.js
@@ -0,0 +1,73 @@
+import { AsyncStorage } from 'react-native';
+
+let darkTheme = {
+    bottomNav: {
+        background: '#1D1D21',
+        activeIcon: 'rgb(255, 255, 255)',
+        inactiveIcon: 'rgba(255, 255, 255, 0.7)',
+        rippleColor: 'rgb(255, 255, 255)'
+    }
+};
+
+let lightTheme = {
+    bottomNav: {
+        background: '#1D1D21',
+        activeIcon: 'rgb(255, 255, 255)',
+        inactiveIcon: 'rgba(255, 255, 255, 0.7)',
+        rippleColor: 'rgb(255, 255, 255)'        
+    }
+};
+
+
+function getTheme(callback){
+    try {
+        AsyncStorage.getItem('currentTheme')
+            .then((currentTheme) => {
+                let theme;
+                if (currentTheme != null) {
+                    switch (currentTheme) {
+                        case 'dark':
+                            theme = darkTheme;
+                            break;
+                        
+                        case 'light':
+                            theme = lightTheme;
+                            break;
+
+                        default:
+                            theme = lightTheme;
+                            break;
+                    }
+                } else {
+                    theme = darkTheme;
+                }
+                callback(theme);
+            })
+    } catch (error) {
+        return 'Error';
+    }
+}
+
+function getThemeName(callback){
+    try {
+        AsyncStorage.getItem('currentTheme')
+            .then((currentTheme) => {
+                callback(currentTheme);
+            })
+    } catch (error) {
+        //error handling
+    }
+}
+
+function setTheme(themeName, callback){
+    try {
+        AsyncStorage.setItem('currentTheme', themeName)
+            .then((newTheme) => {
+                callback(newTheme);
+            });
+    } catch (error) {
+        // Error saving data
+    }
+}
+
+export { getTheme, getThemeName, setTheme };
