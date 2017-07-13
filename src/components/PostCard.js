0bc7343b6e7698b5f35f94eabc7d195f902d93d0
Waquid Valiya Peedikakkal <waquidvp@gmail.com>
2017-07-13 13:10:23 +0100

    Added Post Card Component, Post Card is only a boilerplate now. Mistake from previous commit.

---
+++ src/components/PostCard.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import { withTheme } from 'styled-components';
+
+const MainContainer = styled.View`
+    height: 400px;
+    background-color: white;
+    elevation: 2;
+    margin: 8px 8px;
+    border-radius: 2px;
+`;
+
+const PostContent = styled.View`
+
+`;
+
+const PostDetail = styled.View`
+
+`;
+
+const PostFooter = styled.View`
+    
+`;
+
+class PostCard extends React.Component {
+    render(){
+        return(
+            <MainContainer>
+                <PostContent>
+
+                </PostContent>
+                <PostDetail>
+
+                </PostDetail>
+                <PostFooter>
+
+                </PostFooter>
+            </MainContainer>
+        )
+    }
+}
+
+export default withTheme(PostCard);
