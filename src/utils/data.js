640bb114206570b5c56e0afb24f9970bfd571771
Waquid Valiya Peedikakkal <waquidvp@gmail.com>
2017-08-14 19:49:37 +0530

    Added fake data

---
+++ src/utils/data.js
@@ -0,0 +1,36 @@
+export default {
+    feed: [
+        {
+            key: 1,
+            imgURL: 'https://cdn3.uvnimg.com/dims4/default/87f45b6/2147483647/resize/956x717%3E/quality/75/?url=https%3A%2F%2Fcdn4.uvnimg.com%2F59%2F28%2Febe63b514f979950e3965d8f3149%2FGettyImages-479796634.jpg',
+            user: 'brookspate',
+            caption: 'I\'m the greatest, I said that before I knew I was',
+            tags: [
+                'muhammed',
+                'ali',
+                'takecare',
+                'rip'
+            ],
+            likes: 85643,
+            commentNum: 3450,
+            liked: false
+        },
+        {
+            key: 2,
+            imgURL: 'https://www.tesla.com/sites/default/files/images/homepage/mobile/homepage-hero-us-only--touch@750w.jpg?20170808',
+            user: 'jacksmith',
+            caption: 'The next generation of cars, Long Live Tesla!',
+            tags: [
+                'tesla',
+                'electric',
+                'cars',
+            ],
+            likes: 70638,
+            commentNum: 4903,
+            liked: true
+        },
+    ],
+    messages: [
+
+    ]
+}
