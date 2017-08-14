4954fca0744dfe902da394e41c53b163dac8289d
Waquid Valiya Peedikakkal <waquidvp@gmail.com>
2017-08-14 10:55:38 +0530

    Added icons to bottom navigation

--- src/navigators/MainBottomNavigator.js
+++ src/navigators/MainBottomNavigator.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React from 'react';
-import { NavigationComponent } from 'react-native-material-bottom-navigation'
+import { NavigationComponent } from 'react-native-material-bottom-navigation';
 import { TabNavigator } from 'react-navigation';
 import { withTheme } from 'styled-components';
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -12,54 +12,54 @@ import Chats from '../screens/Chats';
 import Account from '../screens/Account';
 
 const createRootNavigator = (theme) => {
-    return TabNavigator({
-        Feed: { screen: Feed },
-        Search: { screen: Search },
-        Chats: { screen: Chats },
-        Account: { screen: Account }
-    }, {
-        tabBarPosition: 'bottom',
-        swipeEnabled: false,
-        tabBarComponent: NavigationComponent,
-        animationEnabled: false,
-        tabBarOptions: {
-            bottomNavigationOptions: {
-                style: {
-                    borderTopWidth: 0,
-                    elevation: 8,
-                },
-                labelColor: theme.bottomNav.inactiveIcon,
-                rippleColor: theme.bottomNav.rippleColor,
-                backgroundColor: theme.bottomNav.background,
-                activeLabelColor: theme.bottomNav.activeIcon,
-                tabs: {
-                    Feed: {
-                        activeIcon: <Icon size={24} color={theme.bottomNav.activeIcon} name="view-agenda" />
-                    },
-                    Search: {
-                        activeIcon: <Icon size={24} color={theme.bottomNav.activeIcon} name="search" />
-                    },
-                    Chats: {
-                        activeIcon: <Icon size={24} color={theme.bottomNav.activeIcon} name="chat" />
-                    },
-                    Account: {
-                        activeIcon: <Icon size={24} color={theme.bottomNav.activeIcon} name="account-circle" />
-                    }
-                }
-            }
-        }
-    });
+  return TabNavigator({
+    Feed: { screen: Feed },
+    Search: { screen: Search },
+    Chats: { screen: Chats },
+    Account: { screen: Account }
+  }, {
+    tabBarPosition: 'bottom',
+    swipeEnabled: false,
+    tabBarComponent: NavigationComponent,
+    animationEnabled: false,
+    tabBarOptions: {
+      bottomNavigationOptions: {
+        style: {
+          borderTopWidth: 0,
+          elevation: 8,
+        },
+        labelColor: theme.bottomNav.inactiveIcon,
+        rippleColor: theme.bottomNav.rippleColor,
+        backgroundColor: theme.bottomNav.background,
+        activeLabelColor: theme.bottomNav.activeIcon,
+        tabs: {
+          Feed: {
+            activeIcon: <Icon size={24} color={theme.bottomNav.activeIcon} name="view-agenda" />,
+          },
+          Search: {
+            activeIcon: <Icon size={24} color={theme.bottomNav.activeIcon} name="search" />,
+          },
+          Chats: {
+            activeIcon: <Icon size={24} color={theme.bottomNav.activeIcon} name="chat" />,
+          },
+          Account: {
+            activeIcon: <Icon size={24} color={theme.bottomNav.activeIcon} name="account-circle" />,
+          },
+        },
+      },
+    },
+  });
 };
 
-class MainBottomNavigator extends React.Component {    
-    render(){
-        let { theme } = this.props;
-        let Navigator = createRootNavigator(theme);
+class MainBottomNavigator extends React.Component {
+  render() {
+    let { theme } = this.props;
+    const Navigator = createRootNavigator(theme);
 
-        return(
-            <Navigator screenProps={{theme}}/>
-        );
-    }
+    return (
+      <Navigator screenProps={{ theme }} />
+    );
+  }
 }
 
 export default withTheme(MainBottomNavigator);
