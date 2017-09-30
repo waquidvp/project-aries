2eed9e0df49356c1908d196fc295d34f36801bfa
Waquid Valiya Peedikakkal <waquidvp@gmail.com>
2017-09-30 21:34:14 +0100

    Added design fixes: Changed 2px radius to 6 px radius. Changed colors of post card and other color fixes.

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,18 +2,23 @@
 
 import React from 'react';
 import styled from 'styled-components/native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import IconButton from './IconButton';
 
 const MainContainer = styled.View`
     height: 42px;
     background-color: #FFFFFF;
-    border-radius: 2px;
+    border-radius: 6px;
     elevation: 2;
     padding: 0 8px;
     align-items: center;
     justify-content: space-between;
     flex-direction: row;
+`;
+
+const LeftIconContainer = styled.View`
+    padding-left: 8px;
 `;
 
 const SearchInput = styled.TextInput`
@@ -27,10 +32,13 @@ export default class SearchBox extends React.Component {
   render() {
     return (
       <MainContainer>
-        <IconButton
-          name="search"
-          size={40}
-        />
+        <LeftIconContainer>
+          <Icon
+            name="search"
+            size={24}
+            color='#747474'
+          />
+        </LeftIconContainer>
         <SearchInput
           placeholder="Search"
           underlineColorAndroid={'transparent'}
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -7,11 +7,20 @@ let darkTheme = {
   topBar: {
     background: '#212121'
   },
+  text: {
+    primary: 'rgba(255, 255, 255, 1)',
+    secondary: 'rgba(255, 255, 255, 0.7)',
+    disabled: 'rgba(255, 255, 255, 0.5)',
+  },
   card: {
+    background: '#424242',
     text: {
-      primary: 'rgba(0, 0, 0, 0.87)',
-      secondary: 'rgba(0, 0, 0, 0.54)'
+      primary: 'rgba(255, 255, 255, 1)',
+      secondary: 'rgba(255, 255, 255, 0.7)',
     }
+  },
+  chatBubble: {
+    background: '#424242'
   },
   bottomNav: {
     background: '#212121',
@@ -23,15 +32,24 @@ let darkTheme = {
 };
 
 let lightTheme = {
-  background: 'rgba(0, 0, 0, 0.15)',
+  background: 'rgba(0, 0, 0, 0.10)',
   topBar: {
     background: '#F5F5F5'
   },
+  text: {
+    primary: 'rgba(0, 0, 0, 0.87)',
+    secondary: 'rgba(0, 0, 0, 0.54)',
+    disabled: 'rgba(0, 0, 0, 0.38)',
+  },
   card: {
+    background: 'white',
     text: {
       primary: 'rgba(0, 0, 0, 0.87)',
       secondary: 'rgba(0, 0, 0, 0.54)'
     }
+  },
+  chatBubble: {
+    background: 'rgba(0, 0, 0, 0.1)'
   },
   bottomNav: {
     background: '#F5F5F5',
