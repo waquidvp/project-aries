0869c4a301f89d6a6d6e2185cf9d44bb4159dfc6
Waquid Valiya Peedikakkal <waquidvp@gmail.com>
2017-10-06 21:57:40 +0100

    Fixed background color problem after splashscreen added

--- App.js
+++ App.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import styled from 'styled-components/native';
 
 import { getTheme, setTheme } from './src/utils/theme';
 import MainBottomNavigator from './src/navigators/MainBottomNavigator';
@@ -9,38 +10,47 @@ import SplashScreen from './src/screens/SplashScreen';
 
 console.disableYellowBox = true;
 
+const MainContainer = styled.View`
+  flex: 1;
+  background: white;
+`;
+
 export default class App extends React.Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {};
+        this.state = {};
 
-    this.updateTheme = this.updateTheme.bind(this);    
-  }
+        this.updateTheme = this.updateTheme.bind(this);
+    }
 
-  updateTheme(themeName) {
-    setTheme(themeName, () => {
-      let theme = getTheme(theme => {
-        this.setState({ theme: theme });
-      });
-    });
-  }
+    updateTheme(themeName) {
+        setTheme(themeName, () => {
+            let theme = getTheme(theme => {
+                this.setState({ theme: theme });
+            });
+        });
+    }
 
-  componentWillMount() {
-    let theme = getTheme(theme => {
-      this.setState({ theme: theme });
-    });
-  }
+    componentWillMount() {
+        let theme = getTheme(theme => {
+            this.setState({ theme: theme });
+        });
+    }
 
-  render() {
-    return (
-      this.state.theme ?
-        <ThemeProvider theme={this.state.theme}>
-          <MainBottomNavigator
-            props={{ updateTheme: this.updateTheme }}
-          />
-        </ThemeProvider>
-        : <SplashScreen />
-    );
-  }
+    render() {
+        return (
+            <MainContainer>
+                {this.state.theme ? (
+                    <ThemeProvider theme={this.state.theme}>
+                        <MainBottomNavigator
+                            props={{ updateTheme: this.updateTheme }}
+                        />
+                    </ThemeProvider>
+                ) : (
+                    <SplashScreen />
+                )}
+            </MainContainer>
+        );
+    }
 }
