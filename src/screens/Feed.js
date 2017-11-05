0903c706f0dec65b76d89bc7d9d9ca87be4d6737
Waquid Valiya Peedikakkal <waquidvp@gmail.com>
2017-11-05 13:51:52 +0000

    Updated bottom-navigation and implemented scroll to top in feed

--- src/screens/Feed.js
+++ src/screens/Feed.js
@@ -25,106 +25,122 @@ const FABContainer = styled.View`
     bottom: 16px;
 `;
 
+let feedList = null;
+
 class Feed extends React.Component {
     static propTypes = {
-        theme: PropsTypes.object.isRequired
+      theme: PropsTypes.object.isRequired,
     };
 
-    static navigationOptions = ({ screenProps }) => ({
-        tabBarLabel: 'Feed',
-        tabBarIcon: (
-            <Icon
-                size={24}
-                color={screenProps.theme.bottomNav.inactiveIcon}
-                name="view-agenda"
-            />
-        )
+    static navigationOptions = props => ({
+      tabBarLabel: 'Feed',
+      tabBarIcon: (
+        <Icon
+          size={24}
+          color={props.screenProps.theme.bottomNav.inactiveIcon}
+          name="view-agenda"
+        />
+      ),
+      tabBarOnPress: (tab, jumpToIndex) => {
+        if (!tab.focused) {
+          jumpToIndex(tab.index);
+        } else {
+          if (feedList) {
+            feedList.scrollToOffset({ x: 0, y: 0, animated: true });
+          }
+        }
+      },
     });
 
     constructor(props) {
-        super(props);
+      super(props);
 
-        UIManager.setLayoutAnimationEnabledExperimental &&
-            UIManager.setLayoutAnimationEnabledExperimental(true);
+      UIManager.setLayoutAnimationEnabledExperimental &&
+      UIManager.setLayoutAnimationEnabledExperimental(true);
 
-        this.state = {
-            refreshing: false,
-            isActionButtonVisible: true
-        };
+      this.state = {
+        refreshing: false,
+        isActionButtonVisible: true,
+      };
 
-        this.refresh = this.refresh.bind(this);
+      this.refresh = this.refresh.bind(this);
     }
 
     listViewOffset = 0;
 
-    onScroll = event => {
-        // Simple fade-in / fade-out animation
-        const CustomLayoutLinear = {
-            duration: 100,
-            create: {
-                type: LayoutAnimation.Types.linear,
-                property: LayoutAnimation.Properties.opacity
-            },
-            update: {
-                type: LayoutAnimation.Types.linear,
-                property: LayoutAnimation.Properties.opacity
-            },
-            delete: {
-                type: LayoutAnimation.Types.linear,
-                property: LayoutAnimation.Properties.opacity
-            }
-        };
+    onScroll = (event) => {
+      // Simple fade-in / fade-out animation
+      const CustomLayoutLinear = {
+        duration: 100,
+        create: {
+          type: LayoutAnimation.Types.linear,
+          property: LayoutAnimation.Properties.opacity,
+        },
+        update: {
+          type: LayoutAnimation.Types.linear,
+          property: LayoutAnimation.Properties.opacity,
+        },
+        delete: {
+          type: LayoutAnimation.Types.linear,
+          property: LayoutAnimation.Properties.opacity,
+        },
+      };
         // Check if the user is scrolling up or down by confronting the new scroll position with your own one
-        const currentOffset = event.nativeEvent.contentOffset.y;
-        const direction =
+      const currentOffset = event.nativeEvent.contentOffset.y;
+      const direction =
             currentOffset > 0 && currentOffset > this.listViewOffset
-                ? 'down'
-                : 'up';
+              ? 'down'
+              : 'up';
         // If the user is scrolling down (and the action-button is still visible) hide it
-        const isActionButtonVisible = direction === 'up';
-        if (isActionButtonVisible !== this.state.isActionButtonVisible) {
-            LayoutAnimation.configureNext(CustomLayoutLinear);
-            this.setState({ isActionButtonVisible });
-        }
-        // Update your scroll position
-        this.listViewOffset = currentOffset;
+      const isActionButtonVisible = direction === 'up';
+      if (isActionButtonVisible !== this.state.isActionButtonVisible) {
+        LayoutAnimation.configureNext(CustomLayoutLinear);
+        this.setState({ isActionButtonVisible });
+      }
+      // Update your scroll position
+      this.listViewOffset = currentOffset;
     };
 
     refresh() {
-        this.setState({ refreshing: true });
-        setTimeout(() => {
-            this.setState({ refreshing: false });
-        }, 2000);
+      this.setState({ refreshing: true });
+      setTimeout(() => {
+        this.setState({ refreshing: false });
+      }, 2000);
+    }
+
+    componentWillUnmont() {
+      feedList = null;
     }
 
     render() {
-        const { theme } = this.props;
-        const data = demoData.feed;
+      const { theme } = this.props;
+      const data = demoData.feed;
 
-        return (
-            <MainContainer>
-                <FlatList
-                    showsVerticalScrollIndicator={false}
-                    onScroll={this.onScroll}
-                    data={data}
-                    onRefresh={this.refresh}
-                    refreshing={this.state.refreshing}
-                    renderItem={({ item }) => <PostCard post={item} />}
-                    ListHeaderComponent={Header}
-                    ListFooterComponent={ListSeperator}
-                />
-                {this.state.isActionButtonVisible ? (
-                    <FABContainer>
-                        <FAB
-                            name="add"
-                            color={theme.fab.background}
-                            iconColor={theme.fab.fabIcon}
-                            elevation={6}
-                        />
-                    </FABContainer>
-                ) : null}
-            </MainContainer>
-        );
+      return (
+        <MainContainer>
+          <FlatList
+            showsVerticalScrollIndicator={false}
+            onScroll={this.onScroll}
+            data={data}
+            onRefresh={this.refresh}
+            refreshing={this.state.refreshing}
+            renderItem={({ item }) => <PostCard post={item} />}
+            ListHeaderComponent={Header}
+            ListFooterComponent={ListSeperator}
+            ref={(flatList) => { feedList = flatList }}
+          />
+          {this.state.isActionButtonVisible ? (
+            <FABContainer>
+              <FAB
+                name="add"
+                color={theme.fab.background}
+                iconColor={theme.fab.fabIcon}
+                elevation={6}
+              />
+            </FABContainer>
+          ) : null}
+        </MainContainer>
+      );
     }
 }
 
